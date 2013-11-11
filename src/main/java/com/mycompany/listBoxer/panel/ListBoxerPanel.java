9eabf62f3fca627fd8731e89d0158632bd02e80a
LadaVilada <bobina.vlada@gmail.com>
2013-11-11 15:36:42 +0400

    add content for panel

--- src/main/java/com/mycompany/listBoxer/panel/ListBoxerPanel.java
+++ src/main/java/com/mycompany/listBoxer/panel/ListBoxerPanel.java
@@ -1,34 +1,25 @@
 package com.mycompany.listBoxer.panel;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Container;
+import java.awt.GridBagConstraints;
 
-import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 public class ListBoxerPanel extends JPanel {
 
 	private static final long serialVersionUID = -4541078376891317577L;
 
-	private JButton button = new JButton("Action");
-	private JTextArea textarea = new JTextArea(5, 20);
-	private JLabel label = new JLabel("Text Area");
-
 	public ListBoxerPanel() {
 
-		add(label);
-
-		textarea.setEditable(Boolean.FALSE);
-		add(textarea);
-
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent ae) {
-
-			}
-		});
-		add(button);
+	}
+	
+	public static void addComponentsToPanel(Container container){
+		JComboBox rangeCombo;
+		rangeCombo = new JComboBox();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		
 	}
 
 }
