0b70e0dc3292e3da6eaebc15951885cd14a18a3f
Sergey <tsscsf@gmail.com>
2013-11-13 10:27:58 +0400

    Created method getKey() in RangeType

--- src/main/java/com/mycompany/listBoxer/dto/RangeType.java
+++ src/main/java/com/mycompany/listBoxer/dto/RangeType.java
@@ -10,54 +10,50 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- *
+ * 
  * @author vbobina
  */
 public enum RangeType {
 
-    ALL("All"), AM("A-M, a-m"), NZ("N-Z, n-z"),
-    NUM1("0-100"), NUM2("101-200"), NUM3("201-300"), NUM4("301-9999");
+	ALL("All"), AM("A-M, a-m"), NZ("N-Z, n-z"), NUM1("0-100"), NUM2("101-200"), NUM3(
+			"201-300"), NUM4("301-9999");
 
-    private final String key;
+	private final String key;
 
-    private RangeType(String key) {
-        this.key = key;
-    }
+	private RangeType(String key) {
+		this.key = key;
+	}
 
-    public static List<String> getStringValue() {
-    	List<String> result = new ArrayList<String>();
-        List<RangeType> rangeTypeList = Arrays.asList(RangeType.values());
-        for (RangeType type : rangeTypeList)
-        {
-        	result.add(type.key);
-        }
-        return result;
-    }
+	public static List<String> getStringValue() {
+		List<String> result = new ArrayList<String>();
+		List<RangeType> rangeTypeList = Arrays.asList(RangeType.values());
+		for (RangeType type : rangeTypeList) {
+			result.add(type.getKey());
+		}
+		return result;
+	}
 
-    public RangeType getByRange(String key) {
+	public RangeType getByRange(String key) {
 
-        return RangeType.valueOf(key);
+		return RangeType.valueOf(key);
 
-    }
-    
-    public static RangeType fromKey(String rangeTypeKey)
-    {
-        RangeType[] enums = RangeType.values();
+	}
 
-        for (RangeType item : enums)
-        {
-            if (rangeTypeKey != null && item.getKey().equals(rangeTypeKey))
-            {
-                return item;
-            }
-        }
+	public static RangeType fromKey(String rangeTypeKey) {
+		RangeType[] enums = RangeType.values();
 
-        throw new IllegalArgumentException(String.format("Unknown cssClassName %s", RangeType.class));
-    }
-    
-    public String getKey()
-    {
-    	return key;
-    }
-    
+		for (RangeType item : enums) {
+			if (rangeTypeKey != null && item.getKey().equals(rangeTypeKey)) {
+				return item;
+			}
+		}
+
+		throw new IllegalArgumentException(String.format(
+				"Unknown cssClassName %s", RangeType.class));
+	}
+
+	public String getKey() {
+		return key;
+	}
+
 }
